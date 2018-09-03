85ef1b6319041f3a8a0ef18a40dbdf13c7392de0
Sergio <sergio_andres_29@hotmail.com>
2018-09-03 14:22:39 -0500

    the identification of each graphic component was assigned

--- Calculator/src/application/SampleController.java
+++ Calculator/src/application/SampleController.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.awt.TextField;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
@@ -8,6 +10,55 @@ public class SampleController {
 	
 	@FXML
 	private Button simply;
+	@FXML
+	private Button inverse;
+	@FXML
+	private Button square;
+	@FXML
+	private Button nine;
+	@FXML
+	private Button eigth;
+	@FXML
+	private Button seven;
+	@FXML
+	private Button six;
+	@FXML
+	private Button five;
+	@FXML
+	private Button four;
+	@FXML
+	private Button three;
+	@FXML
+	private Button two;
+	@FXML
+	private Button one;
+	@FXML
+	private Button zero;
+	@FXML
+	private Button adding;
+	@FXML
+	private Button substract;
+	@FXML
+	private Button diving;
+	@FXML
+	private Button multiplying;
+	@FXML
+	private Button iquals;
+	
+	@FXML
+	private TextField num1;
+	@FXML
+	private TextField num2;
+	@FXML
+	private TextField num3;
+	@FXML
+	private TextField num4;
+	@FXML
+	private TextField operation;
+	@FXML
+	private TextField result1;
+	@FXML
+	private TextField result2;
 	
 	
 	
